ddfcd584121d61727606d9e518d243c986c868bc
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-09-21 21:48:45 +0700

    feat(sponsor-us): Add decorations

--- resources/views/components/container.blade.php
+++ resources/views/components/container.blade.php
@@ -1,4 +1,10 @@
-<div class="container-{{ $level }}">
+<div class="container-{{ $level }} {{ isset($show_decorations) ? 'position-relative' : '' }} {{ $class ?? '' }}" id="{{ $id ?? '' }}">
+    @if(isset($show_decorations) && is_array($show_decorations) && count($show_decorations) > 0)
+        @foreach ($show_decorations as $decor)
+            @component('components.imagecompat', ['src' => '/img/accessories/2021/' . $decor . '.png', 'class' => $decor])
+            @endcomponent
+        @endforeach
+    @endif
     @if(isset($children))
         @foreach($children as $widget)
             @component('functions.parse-widget', ['widget' => $widget])
--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -8,17 +8,19 @@
         @component('components.meta', ['title' => $title])
         @endcomponent
     </head>
-    <body class="is-bootstrap">
+    <body class="is-bootstrap" style="overflow-x: hidden">
         @if(isset($children))
             @foreach($children as $widget)
                 @component('functions.parse-widget', ['widget' => $widget])
                 @endcomponent
             @endforeach
         @endif
-        <div class="container-0">
+        <div class="container-0 pb-0">
             @component('components.sponsors')
             @endcomponent
             <img class="container-clip for-footer" src="/img/backgrounds/7.png">
+            @component('components.imagecompat', ['src' => '/img/accessories/2021/wave-end-1.svg', 'class' => 'w-100 p-0 mt-5 mb-0'])
+            @endcomponent
         </div>
         @component('components.footer')
         @endcomponent
