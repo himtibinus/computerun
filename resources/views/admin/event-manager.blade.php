0b4e259828ceef44ce5c20a42d70622a12a9d969
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-10-07 22:43:02 +0700

    Fix: Admin detection on event manager

--- resources/views/admin/event-manager.blade.php
+++ resources/views/admin/event-manager.blade.php
@@ -1,11 +1,14 @@
- @extends('layouts.app')
-
+@extends('layouts.app')
 @section('content')
+<?php
+  $admin = false;
+  if (Auth::user()->university_id == 2) $admin = true;
+?>
 <form action="/admin/event/{{ $event->id }}" method="POST" class="row flex-row-reverse justify-content-center mx-0">
     @csrf
     <input name="_method" type="hidden" value="PUT">
     <div class="col-12 col-xl-4 p-4 pr-sm-0">
-        @if($role->admin == true)
+        @if($admin == true)
             <h1 class="display-4 fw-bolder mb-4">Settings</h1>
         @else
             <h1 class="display-4 fw-bolder mb-4">Summary</h1>
@@ -38,27 +41,27 @@
             <div class="card-body text-dark">
                 <div class="form-group mb-4">
                     <label for="action-update-kicker">{{ __('Kicker') }}</label>
-                    <input name="action-update-kicker" id="action-update-kicker" type="text" value="{{ $event->kicker }}" class="form-control" @if(!$role->admin) disabled @endif>
+                    <input name="action-update-kicker" id="action-update-kicker" type="text" value="{{ $event->kicker }}" class="form-control" @if(!$admin) disabled @endif>
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-name">{{ __('Event Name') }}<b class="text-danger">*</b></label>
-                    <input name="action-update-name" id="action-update-name" type="text" value="{{ $event->name }}" class="form-control" @if(!$role->admin) disabled @else required @endif>
+                    <input name="action-update-name" id="action-update-name" type="text" value="{{ $event->name }}" class="form-control" @if(!$admin) disabled @else required @endif>
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-date">{{ __('Event Date') }}<b class="text-danger">*</b></label>
-                    <input name="action-update-date" id="action-update-location" type="datetime-date" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\+[0-9]{2}:[0-9]{2}" value="{{ date("c", strtotime($event->date)) }}" class="form-control" @if(!$role->admin) disabled @else required @endif>
+                    <input name="action-update-date" id="action-update-location" type="datetime-date" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\+[0-9]{2}:[0-9]{2}" value="{{ date("c", strtotime($event->date)) }}" class="form-control" @if(!$admin) disabled @else required @endif>
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-location">{{ __('Event Location') }}</label>
-                    <input name="action-update-location" id="action-update-location" type="text" value="{{ $event->location }}" class="form-control" @if(!$role->admin) disabled @endif>
+                    <input name="action-update-location" id="action-update-location" type="text" value="{{ $event->location }}" class="form-control" @if(!$admin) disabled @endif>
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-price">{{ __('Event Price (Rp)') }}<b class="text-danger">*</b></label>
-                    <input name="action-update-price" id="action-update-price" type="number" min="0" value="{{ $event->price }}" class="form-control" @if(!$role->admin) disabled @else required @endif>
+                    <input name="action-update-price" id="action-update-price" type="number" min="0" value="{{ $event->price }}" class="form-control" @if(!$admin) disabled @else required @endif>
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-cover_image">{{ __('Cover Image (URL)') }}</label>
-                    <input name="action-update-cover_image" id="action-update-cover_image" type="text" value="{{ $event->cover_image }}" class="form-control" @if(!$role->admin) disabled @endif>
+                    <input name="action-update-cover_image" id="action-update-cover_image" type="text" value="{{ $event->cover_image }}" class="form-control" @if(!$admin) disabled @endif>
                 </div>
             </div>
         </div>
@@ -69,7 +72,7 @@
             <div class="card-body text-dark">
                 <p>This description will be publicly visible towards all users, registered or not. <b>Markdown is supported and enabled by default.</b></p>
                 <div class="form-group mb-4">
-                    <textarea name="action-update-description_public" id="action-update-description_public" type="text" rows="4" class="form-control" @if(!$role->admin) disabled @endif>{{ $event->description_public }}</textarea>
+                    <textarea name="action-update-description_public" id="action-update-description_public" type="text" rows="4" class="form-control" @if(!$admin) disabled @endif>{{ $event->description_public }}</textarea>
                 </div>
             </div>
         </div>
@@ -80,7 +83,7 @@
             <div class="card-body text-dark">
                 <p>This description will be visible to users who has been recently registered, such as placing payment information. <b>Markdown is supported and enabled by default.</b></p>
                 <div class="form-group mb-4">
-                    <textarea name="action-update-description_pending" id="action-update-description_pending" type="text" rows="4" class="form-control" @if(!$role->admin) disabled @endif>{{ $event->description_pending }}</textarea>
+                    <textarea name="action-update-description_pending" id="action-update-description_pending" type="text" rows="4" class="form-control" @if(!$admin) disabled @endif>{{ $event->description_pending }}</textarea>
                 </div>
             </div>
         </div>
@@ -91,7 +94,7 @@
             <div class="card-body text-dark">
                 <p>This description will be visible to users who has been accepted to the event. <b>Markdown is supported and enabled by default.</b></p>
                 <div class="form-group mb-4">
-                    <textarea name="action-update-description_private" id="action-update-description_private" type="text" rows="4" class="form-control" @if(!$role->admin) disabled @endif>{{ $event->description_private }}</textarea>
+                    <textarea name="action-update-description_private" id="action-update-description_private" type="text" rows="4" class="form-control" @if(!$admin) disabled @endif>{{ $event->description_private }}</textarea>
                 </div>
             </div>
         </div>
@@ -102,7 +105,7 @@
             <div class="card-body text-dark">
                 <div class="form-group mb-4">
                     <label for="action-registration-status"><b>Open for New Registrations?</b> (Current: {{($event->opened) ? 'Yes' : 'No'}})</label><br>
-                    <select name="action-registration-status" id="action-registration-status" @if(!$role->admin) disabled @endif>
+                    <select name="action-registration-status" id="action-registration-status" @if(!$admin) disabled @endif>
                         <option value="-1">Unchanged</option>
                         <option value="enabled">Yes</option>
                         <option value="disabled">No</option>
@@ -110,7 +113,7 @@
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-registration-private"><b>Event Visibility</b> (Current: {{($event->private) ? 'Private' : 'Public'}})</label><br>
-                    <select name="action-registration-private" id="action-registration-private" @if(!$role->admin) disabled @endif>
+                    <select name="action-registration-private" id="action-registration-private" @if(!$admin) disabled @endif>
                         <option value="-1">Unchanged</option>
                         <option value="private">Private</option>
                         <option value="public">Public</option>
@@ -118,7 +121,7 @@
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-registration-auto_accept"><b>Automatically accept new participants?</b> (Current: {{($event->auto_accept) ? 'Yes' : 'No'}})</label><br>
-                    <select name="action-registration-auto_accept" id="action-registration-auto_accept" @if(!$role->admin) disabled @endif>
+                    <select name="action-registration-auto_accept" id="action-registration-auto_accept" @if(!$admin) disabled @endif>
                         <option value="-1">Unchanged</option>
                         <option value="enabled">Yes</option>
                         <option value="disabled">No</option>
@@ -126,19 +129,19 @@
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-seats">{{ __('Maximum Available Seats') }}</label>
-                    <input name="action-update-seats" id="action-update-seats" type="number" value="{{ $event->seats }}" min="1" class="form-control" @if(!$role->admin) disabled @else required @endif>
+                    <input name="action-update-seats" id="action-update-seats" type="number" value="{{ $event->seats }}" min="1" class="form-control" @if(!$admin) disabled @else required @endif>
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-slots">{{ __('Maximum Tickets per User') }}</label>
-                    <input name="action-update-slots" id="action-update-slots" type="number" value="{{ $event->slots }}" min="1" class="form-control" @if(!$role->admin) disabled @else required @endif>
+                    <input name="action-update-slots" id="action-update-slots" type="number" value="{{ $event->slots }}" min="1" class="form-control" @if(!$admin) disabled @else required @endif>
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-team_members">{{ __('Required Team Members') }}</label>
-                    <input name="action-update-team_members" id="action-update-team_members" type="number" value="{{ $event->team_members }}" min="0" class="form-control" @if(!$role->admin) disabled @else required @endif>
+                    <input name="action-update-team_members" id="action-update-team_members" type="number" value="{{ $event->team_members }}" min="0" class="form-control" @if(!$admin) disabled @else required @endif>
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-team_members_reserve">{{ __('Reserve Team Members') }}</label>
-                    <input name="action-update-team_members_reserve" id="action-update-team_members_reserve" type="number" value="{{ $event->team_members_reserve }}" min="0" class="form-control" @if(!$role->admin) disabled @else required @endif>
+                    <input name="action-update-team_members_reserve" id="action-update-team_members_reserve" type="number" value="{{ $event->team_members_reserve }}" min="0" class="form-control" @if(!$admin) disabled @else required @endif>
                 </div>
             </div>
         </div>
@@ -149,7 +152,7 @@
             <div class="card-body text-dark">
                 <div class="form-group mb-4">
                     <label for="action-attendance-status"><b>Open for Attendance?</b> (Current: {{($event->attendance_opened) ? 'Yes' : 'No'}})</label><br>
-                    <select name="action-attendance-status" id="action-attendance-status" @if(!$role->admin) disabled @endif>
+                    <select name="action-attendance-status" id="action-attendance-status" @if(!$admin) disabled @endif>
                         <option value="-1">Unchanged</option>
                         <option value="enabled">Yes</option>
                         <option value="disabled">No</option>
@@ -157,7 +160,7 @@
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-attendance-type"><b>Attendance Type</b> (Current: {{($event->attendance_is_exit) ? 'Exit' : 'Entrance'}})</label><br>
-                    <select name="action-attendance-type" id="action-attendance-type" @if(!$role->admin) disabled @endif>
+                    <select name="action-attendance-type" id="action-attendance-type" @if(!$admin) disabled @endif>
                         <option value="-1">Unchanged</option>
                         <option value="entrance">Entrance</option>
                         <option value="exit">Exit</option>
@@ -165,15 +168,15 @@
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-url_link"><b>{{ __('URL for Online Participants') }}</b><br>Participants will be redirected to this link after check in.</label>
-                    <input name="action-update-url_link" id="action-update-url_link" type="text" value="{{ $event->url_link }}" class="form-control" @if(!$role->admin) disabled @endif>
+                    <input name="action-update-url_link" id="action-update-url_link" type="text" value="{{ $event->url_link }}" class="form-control" @if(!$admin) disabled @endif>
                 </div>
                 <div class="form-group mb-4">
                     <label for="action-update-totp_key"><b>{{ __('Event Check Out Token') }}</b><br>Online participants will be propted to enter the following token to check out.</label>
-                    <input name="action-update-totp_key" id="action-update-totp_key" type="text" value="{{ $event->totp_key }}" class="form-control" @if(!$role->admin) disabled @endif>
+                    <input name="action-update-totp_key" id="action-update-totp_key" type="text" value="{{ $event->totp_key }}" class="form-control" @if(!$admin) disabled @endif>
                 </div>
             </div>
         </div>
-        @if($role->admin == true)
+        @if($admin == true)
             <div class="card mb-4">
                 <div class="card-header h4 text-white bg-primary">
                     <i class="bi bi-check-circle"></i> Confirmation
@@ -271,7 +274,7 @@
             </div>
         </div>
         @endforeach
-        @if($role->admin == true)
+        @if($admin == true)
             <div class="card mb-4">
                 <div class="card-header h4 text-white bg-primary">
                     <i class="bi bi-check-circle"></i> Confirmation
