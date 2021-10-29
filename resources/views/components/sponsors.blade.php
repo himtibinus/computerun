ad2b38b4a5da382de99a301558761427854249e1
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-10-29 19:43:20 +0700

    feat: Add new sponsors

---
+++ resources/views/components/sponsors.blade.php
@@ -0,0 +1,10 @@
+<h1 class="full-underline content-divider">our partners</h1>
+<h3 class="text-center">SPONSORS</h3>
+<div class="content-divider-short text-center margin-1">
+    <div class="sponsors-container large">
+        @component ('components.sponsor-item', ['id' => 'dewaweb', 'name' => 'Dewaweb'])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'why-apparel', 'name' => 'W.H.Y Apparel'])
+        @endcomponent
+    </div>
+</div>
