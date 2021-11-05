b3732ea153ffe0c4a60626b31a786a594ed8fa79
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-11-05 22:03:55 +0700

    feat: Add media partners

--- resources/views/components/sponsors.blade.php
+++ resources/views/components/sponsors.blade.php
@@ -10,3 +10,42 @@
         @endcomponent
     </div>
 </div>
+<h3 class="text-center">MEDIA PARTNERS</h3>
+<div class="content-divider-short text-center margin-1">
+    <div class="sponsors-container small">
+        @component ('components.sponsor-item', ['id' => 'auiss', 'name' => 'Asia Pacific University Indonesian Students Society (AUISS)', 'square' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'bncc', 'name' => 'Bina Nusantara Computer Club (BNCC)', 'square' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'dunia-coding', 'name' => 'Dunia Coding', 'square' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'filemagz', 'name' => 'Filemagz'])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'girlskode', 'name' => 'Girls Kode', 'baseline' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'harisenin', 'name' => 'harisenin.com', 'baseline' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'infolomba', 'name' => 'Info Lomba', 'square' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'kampus-update', 'name' => 'KampusUpdate.com', 'baseline' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'mave-magz', 'name' => 'MAVE magz'])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'pengabdi-sat', 'name' => 'Pengabdi SAT', 'square' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'ppi-delft', 'name' => 'PPI Delft'])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'ppi-taiwan', 'name' => 'PPI Taiwan'])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'radio-kampus-itb', 'name' => 'Radio Kampus ITB', 'baseline' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'radio-ppi-dunia', 'name' => 'Radio Kampus ITB', 'baseline' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'ruang-event', 'name' => 'Ruang Event', 'baseline' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'ruang-mahasiswa', 'name' => 'Ruang Mahasiswa', 'baseline' => true])
+        @endcomponent
+        @component ('components.sponsor-item', ['id' => 'ziliun', 'name' => 'Ziliun', 'baseline' => true])
+        @endcomponent
+    </div>
+</div>
